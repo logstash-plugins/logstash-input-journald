531435d5611f1a0ec9ec242865ac5d08b1f4122e
Stuart Warren <stuart.warren@ocado.com>
2014-12-10 16:47:13 +0000

    remove contents of specfile

--- spec/inputs/journald_spec.rb
+++ spec/inputs/journald_spec.rb
@@ -1,23 +1,12 @@
 # encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
 require "socket"
-require "logstash/inputs/stdin"
+require "logstash/inputs/journald"
 
-describe LogStash::Inputs::Stdin do
-  context "codec (PR #1372)" do
-    it "switches from plain to line" do
-      require "logstash/codecs/plain"
-      require "logstash/codecs/line"
-      plugin = LogStash::Inputs::Stdin.new("codec" => LogStash::Codecs::Plain.new)
-      plugin.register
-      insist { plugin.codec }.is_a?(LogStash::Codecs::Line)
+describe LogStash::Inputs::Journald do
+    context "Ummm" do
+        it "no tests here" do
+
+        end
     end
-    it "switches from json to json_lines" do
-      require "logstash/codecs/json"
-      require "logstash/codecs/json_lines"
-      plugin = LogStash::Inputs::Stdin.new("codec" => LogStash::Codecs::JSON.new)
-      plugin.register
-      insist { plugin.codec }.is_a?(LogStash::Codecs::JSONLines)
-    end
-  end
 end
