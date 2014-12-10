e94d4639981c33c17d435b29aba10dec523b63bb
Stuart Warren <stuart.warren@ocado.com>
2014-12-10 16:39:34 +0000

    initial hack

---
+++ lib/logstash/inputs/journald.rb
@@ -0,0 +1,86 @@
+# encoding: utf-8
+require "logstash/inputs/threadable"
+require "logstash/namespace"
+require "socket"
+require "systemd/journal"
+
+# Pull events from a local systemd journal.
+#
+# See requirements https://github.com/ledbettj/systemd-journal
+class LogStash::Inputs::Journald < LogStash::Inputs::Threadable
+
+    config_name "journald"
+    milestone 1
+
+    # Where in the journal to start capturing logs
+    # Options: head, tail
+    config :seekto, :validate => [ "head", "tail" ], :default => "tail"
+
+    # System journal flags
+    # 0 = all avalable
+    # 1 = local only
+    # 2 = runtime only
+    # 4 = system only
+    #
+    config :flags, :validate => [0, 1, 2, 4], :default => 0
+
+    # Path to read journal files from
+    #
+    config :path, :validate => :string, :default => "/var/log/journal"
+
+    # Filter on events. Not heavily tested.
+    #
+    config :filter, :validate => :hash, :required => false, :default => {}
+
+    # Lowercase annoying UPPERCASE fieldnames. (May clobber existing fields)
+    #
+    config :lowercase, :validate => :boolean, :default => false
+
+    public
+    def register
+        opts = {
+            flags: @flags,
+            path: @path,
+        }
+        @hostname = Socket.gethostname
+        @journal = Systemd::Journal.new(opts)
+    end
+
+    def run(queue)
+        @journal.seek(@seekto.to_sym)
+        @journal.filter(@filter)
+        @journal.watch do |entry|
+            timestamp = entry.realtime_timestamp
+            event = LogStash::Event.new(
+                entry.to_h_lower(@lowercase).merge(
+                    "@timestamp" => timestamp,
+                    "host" => entry._hostname || @hostname,
+                    "cursor" => @journal.cursor
+                )
+            )
+            decorate(event)
+            queue << event
+        end
+    end # def run
+
+    public
+    def teardown
+        @logger.debug("journald shutting down.")
+        @journal = nil
+        finished
+    end # def teardown
+
+end # class LogStash::Inputs::Journald
+
+# Monkey patch Systemd::JournalEntry
+module Systemd
+    class JournalEntry
+        def to_h_lower(is_lowercase)
+            if is_lowercase
+                @entry.each_with_object({}) { |(k, v), h| h[k.downcase] = v.dup }
+            else
+                @entry.each_with_object({}) { |(k, v), h| h[k] = v.dup }
+            end
+        end
+    end
+end
---
+++ logstash-input-journald.gemspec
@@ -0,0 +1,32 @@
+Gem::Specification.new do |s|
+
+  s.name            = 'logstash-input-journald'
+  s.version         = '0.0.1'
+  s.licenses        = ['Apache License (2.0)']
+  s.summary         = "Read events from local systemd journal"
+  s.description     = "This gem is a logstash plugin required to be installed on top of the Logstash core pipeline using $LS_HOME/bin/plugin install gemname. This gem is not a stand-alone program"
+  s.authors         = ["Elasticsearch"]
+  s.email           = 'info@elasticsearch.com'
+  s.homepage        = "http://www.elasticsearch.org/guide/en/logstash/current/index.html"
+  s.require_paths = ["lib"]
+
+  # Files
+  s.files = `git ls-files`.split($\)+::Dir.glob('vendor/*')
+
+  # Tests
+  s.test_files = s.files.grep(%r{^(test|spec|features)/})
+
+  # Special flag to let us know this is actually a logstash plugin
+  s.metadata = { "logstash_plugin" => "true", "logstash_group" => "input" }
+
+  # Gem dependencies
+  s.add_runtime_dependency 'logstash', '>= 1.4.0', '< 2.0.0'
+
+  s.add_runtime_dependency 'logstash-codec-plain'
+  s.add_runtime_dependency 'logstash-codec-line'
+  s.add_runtime_dependency 'logstash-codec-json'
+  s.add_runtime_dependency 'logstash-codec-json_lines'
+  s.add_runtime_dependency 'ffi'
+  s.add_runtime_dependency 'systemd-journal', '~> 1.2.2'
+  s.add_development_dependency 'logstash-devutils'
+end
